1e2697108ed6d8681bdc9fae3202d0a23c9521ee
ElPedron <pedrohenriquesilva@aluno.senai.br>
2023-10-07 20:46:20 -0300

    feat: title e basic css

--- app/page.jsx
+++ app/page.jsx
@@ -4,6 +4,14 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <>
+    <header></header>
+    <main className={styles.container}>
+      <div className={styles.title}>
+        <h1>É OS D</h1> 
+        <h1>EV</h1>
+      </div>
+
+    </main>
     </>
   );
 }
